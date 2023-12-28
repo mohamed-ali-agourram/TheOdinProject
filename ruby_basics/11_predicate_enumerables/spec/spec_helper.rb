17ca5961c0321e8ddc2add7105daf0a0df724521
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-28 18:03:19 +0100

    predicate_enumerable_exercises

--- ruby_basics/11_predicate_enumerables/spec/spec_helper.rb
+++ ruby_basics/11_predicate_enumerables/spec/spec_helper.rb
@@ -6,13 +6,5 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
-
-module FormatterOverrides
-  def dump_pending(_)
-  end
-end
-
-RSpec::Core::Formatters::DocumentationFormatter.prepend FormatterOverrides
